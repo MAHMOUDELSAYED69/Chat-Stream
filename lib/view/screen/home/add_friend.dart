f5870458a82f44ad1f1cbd591d42f82630865d8e
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-02-05 20:58:55 +0200

    refactor add friend card and display it in add friend screen.

--- lib/view/screen/home/add_friend.dart
+++ lib/view/screen/home/add_friend.dart
@@ -9,34 +9,35 @@ class AddFriend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyColors.darkGrey,
-      appBar: AppBar(
-        elevation: BorderSide.strokeAlignOutside,
-        backgroundColor: MyColors.purple,
-        shadowColor: MyColors.darkGrey,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25))),
-        title: const Text("Add Friends",
-            style:
-                TextStyle(fontWeight: FontWeight.w500, color: MyColors.black)),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: MyColors.black,
-                size: 30,
-              )),
-        ],
-      ),
-      body: const Column(children: [
-        AddFriendCard(
-          circleAvatar: "",
-          name: "name",
-        )
-      ]),
-    );
+        backgroundColor: MyColors.darkGrey,
+        appBar: AppBar(
+          elevation: BorderSide.strokeAlignOutside,
+          backgroundColor: MyColors.purple,
+          shadowColor: MyColors.darkGrey,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25))),
+          title: const Text("Add Friends",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500, color: MyColors.black)),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: MyColors.black,
+                  size: 30,
+                )),
+          ],
+        ),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: 20,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) =>
+              const AddFriendCard(circleAvatar: "M", name: "Mahmoud El Sayed"),
+        ));
   }
 }
--- lib/view/widget/custom_add_friend_card.dart
+++ lib/view/widget/custom_add_friend_card.dart
@@ -22,65 +22,62 @@ class _AddFriendCardState extends State<AddFriendCard> {
   bool isTap = false;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: widget.onTap,
-      child: Container(
-        width: 170,
-        margin: const EdgeInsets.all(5),
-        padding: const EdgeInsets.all(10),
-        decoration: const BoxDecoration(
-            color: MyColors.darkGrey2,
-            border: Border.symmetric(
-                horizontal: BorderSide(width: 2, color: MyColors.black))),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: MyColors.darkGrey,
-              child: Text(
-                widget.circleAvatar,
-                style: const TextStyle(fontSize: 20, color: MyColors.white),
-              ),
+    return Container(
+      margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      decoration: const BoxDecoration(
+          color: MyColors.darkGrey2,
+          border: Border.symmetric(
+              horizontal: BorderSide(width: 2, color: MyColors.black))),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: MyColors.darkGrey,
+            child: Text(
+              widget.circleAvatar,
+              style: const TextStyle(fontSize: 20, color: MyColors.white),
             ),
-            const SizedBox(height: 20),
-            Text(
-              widget.name,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                  fontSize: 24,
-                  color: MyColors.white,
-                  fontWeight: FontWeight.w500),
-            ),
-            CustomButton(
-              color: isTap ? MyColors.lightGrey : MyColors.purple,
-              height: 45,
-              widget: isTap
-                  ? const Text(
-                      "Send",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  : const Text(
-                      "Add Friend",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            widget.name,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+                fontSize: 18,
+                color: MyColors.white,
+                fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            color: isTap ? MyColors.lightGrey : MyColors.purple,
+            height: 40,
+            widget: isTap
+                ? const Text(
+                    "Send",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
                     ),
-              fontSize: 18,
-              onPressed: () {
-                isTap = !isTap;
-                setState(() {});
-                widget.onTap;
-              },
-            )
-          ],
-        ),
+                  )
+                : const Text(
+                    "Add Friend",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+            fontSize: 18,
+            onPressed: () {
+              isTap = !isTap;
+              setState(() {});
+              widget.onTap;
+            },
+          )
+        ],
       ),
     );
   }
